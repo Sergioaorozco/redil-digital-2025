ab44df90fedd457ac034c40ad6801a749c512ace
Sergioaorozco <sergiorozco.ochoa@gmail.com>
2025-04-14 22:36:09 -0500

    Adding Login button

--- src/middleware/index.ts
+++ src/middleware/index.ts
@@ -1,16 +1,34 @@
+import type { MiddlewareNext } from "astro";
 import { defineMiddleware } from "astro:middleware";
 
 const privateRoutes = ['/membresia'];
 
 export const onRequest = defineMiddleware(async ({url, request}, next) => {
 
+    const authHeaders = request.headers.get('Authorization') ?? '';
+
     if(privateRoutes.includes(url.pathname)){
-        return new Response('Autorizacion Requerida', {
-            status: 401,
-            headers: {
-                'WWW-Authenticate': 'Basic realm="Secure Area"'
-            }
-        })
+        return checkLocalAuth(authHeaders, next )
     }
     return next();
 })
+
+const checkLocalAuth = ( authHeaders:string, next:MiddlewareNext) => {
+    if(authHeaders) {
+        const authValue = authHeaders.split(' ').at(-1) ?? 'user:pass';
+        const decodedValue = atob(authValue).split(':');
+        const [user, pass] = decodedValue;
+    
+        if(user === 'admin' && pass === '1234') {
+            console.log(user, pass);
+            return next();
+        }
+    }
+
+    return new Response('Autorizacion Requerida', {
+        status: 401,
+        headers: {
+            'WWW-Authenticate': 'Basic realm="Secure Area"'
+        }
+    })
+}
