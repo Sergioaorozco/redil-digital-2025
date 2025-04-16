c67ffb5c8f76dc78f458ae8301f9736fe452e849
sorozco <sergiorozco.ochoa@gmail.com>
2025-04-15 23:29:50 -0500

    Storing name in cookies for upcoming visit

--- src/actions/auth/register.action.ts
+++ src/actions/auth/register.action.ts
@@ -6,12 +6,21 @@ export const loginUser= defineAction({
   accept: 'form',
   input: z.object({
     name: z.string(),
-    email: z.string().email(),
-    rememberMe: z.boolean().optional(),
+    email: z.string().email().optional(),
+    remember_me: z.boolean().optional(),
     password: z.string()
   }),
-  handler: async ({name, password, email, rememberMe}) => {
-    console.log(name, password, email, rememberMe);
-    return true;
+  handler: async ({name, password, email, remember_me}, {cookies}) => {
+    if(remember_me) {
+      cookies.set('name', name, {
+        expires: new Date(Date.now() + 1000 * 60 * 60 * 24 * 365),
+        path: '/'
+      });
+    } else {
+      cookies.delete('name', {
+        path: '/',
+      });
+    }
+    return { status: 'success', message: 'Ingreso Correcto'};
   }
 })
