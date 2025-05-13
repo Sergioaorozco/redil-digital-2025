1b4d456c89c33eaef1131824e5e07f7f857a753c
sorozco <sergiorozco.ochoa@gmail.com>
2025-05-13 13:40:27 -0500

    Fixing middleware to redirect to login view

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,9 +3,10 @@ import { clerkMiddleware, createRouteMatcher } from '@clerk/astro/server'
 const isProtectedRoute = createRouteMatcher(['/miembro'])
 
 export const onRequest = clerkMiddleware((auth, context) => {
-  const { redirectToSignIn, userId } = auth()
+  const { userId } = auth()
 
   if (!userId && isProtectedRoute(context.request)) {
-    return redirectToSignIn();
+    return context.redirect('ingreso');
   }
+
 })
