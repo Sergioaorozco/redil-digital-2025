de2d9820679967b42cc6092c54f47991af39e672
sorozco <sergiorozco.ochoa@gmail.com>
2025-06-30 17:27:12 -0500

    Adding vue clerk sign component

---
+++ src/components/islands/ClerkSign.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { SignedOut, SignIn} from "@clerk/vue";
+
+
+</script>
+<template>
+  <SignedOut>
+    <section class="relative flex justify-center items-center">
+      <SignIn oauthFlow="auto"/>
+    </section>
+  </SignedOut>
+</template>
