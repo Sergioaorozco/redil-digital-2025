3e966f0090b591ff103a5ec67b85cb86212dca92
sorozco <sergiorozco.ochoa@gmail.com>
2025-06-30 17:52:45 -0500

    Fix: dependencies and translations

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import { esUY } from '@clerk/localizations';
+import { esES, esUY } from '@clerk/localizations';
 
 import tailwindcss from '@tailwindcss/vite';
 import clerk from "@clerk/astro"
 import node from '@astrojs/node';
-import vue from '@astrojs/vue';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [clerk({localization: esUY}), vue()],
+  integrations: [clerk({localization: esES})],
   adapter: node({ mode: "standalone" }),
   output: "server",
 
