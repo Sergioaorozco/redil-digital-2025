447433e94d134e15a8879d2824eea986e52abaaa
sorozco <sergiorozco.ochoa@gmail.com>
2025-08-20 21:55:12 -0500

    Removing Clerk auth from the project.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import { esES } from '@clerk/localizations';
-
-import clerk from "@clerk/astro"
-
 import vercel from "@astrojs/vercel";
 import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [clerk({localization: esES})],
   output: "server",
   adapter: vercel(),
 
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,12 +1,2 @@
-import { clerkMiddleware, createRouteMatcher } from '@clerk/astro/server'
-
-const isProtectedRoute = createRouteMatcher(['/miembros(.*)'])
-
-export const onRequest = clerkMiddleware((auth, context) => {
-  const { userId } = auth()
-
-  if (!userId && isProtectedRoute(context.request)) {
-    return context.redirect('ingreso');
-  }
-
-})
+import { defineMiddleware } from "astro:middleware";
+const isProtectedRoute = ['/miembros(.*)'];
