c4c229c537e23fbfef1b0d920a5247ad176bb5be
sorozco <sergiorozco.ochoa@gmail.com>
2025-08-22 19:04:56 -0500

    Installing lucide icons and placing Astro locals

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,12 +1,21 @@
 import { defineMiddleware } from "astro:middleware";
+import { firebaseApp } from "./firebase/config";
 
 const isProtectedRoute = ['/miembros(.*)'];
+const loggedInRoutes = ['/ingreso'];
 
 export const onRequest = defineMiddleware(async ({url, locals, redirect}, next) => {
+  const isLoggedIn = !!firebaseApp.auth.currentUser;
 
+  locals.isLoggedIn = isLoggedIn;
+  
   // Check if no logged user is trying to access a protected route
-  if ( isProtectedRoute.some(route => url.pathname.match(route))) {
+  if ( !isLoggedIn && isProtectedRoute.some(route => url.pathname.match(route))) {
     return redirect('/ingreso');
+  }
+
+  if (isLoggedIn && loggedInRoutes.some(route => url.pathname.match(route))) {
+    return redirect('/miembros')
   }
 
   return next();
