e89688a17ea82d46d865b5b055e1f181fbc3e47d
sorozco <sergiorozco.ochoa@gmail.com>
2025-08-25 21:38:11 -0500

    Adding react library to install shadcn components.

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,8 @@ import { defineConfig } from 'astro/config';
 import vercel from "@astrojs/vercel";
 import tailwindcss from "@tailwindcss/vite";
 
+import react from "@astrojs/react";
+
 // https://astro.build/config
 export default defineConfig({
   output: "server",
@@ -11,4 +13,6 @@ export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
+
+  integrations: [react()],
 });
