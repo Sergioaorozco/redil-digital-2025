e3d3062165d3471b58bbce4e71208ae3e0d1f73d
Sergioaorozco <sergiorozco.ochoa@gmail.com>
2025-11-09 21:33:12 -0500

    Improve animations based GPU properties

--- src/utils/gsapAnimations.ts
+++ src/utils/gsapAnimations.ts
@@ -6,40 +6,35 @@ export const createmenuAnim = (modalMenu: HTMLElement) => {
   const bgImage = modalMenu.querySelector('.background-nav-image');
   const menuItems = modalMenu.querySelectorAll('.menu-list li');
   const $modal = document.getElementById("modalMenu");
-  const $backgroundNav = document.querySelector(".background-nav-image");
 
-  // Inital Setup
-  gsap.set($modal, {x:"100%", autoAlpha:0});
-  gsap.set($backgroundNav, {width:"0%"});
+  // Initial Setup
+  gsap.set($modal, { autoAlpha: 0 }); // Fade in the modal
+  gsap.set(bgImage, { autoAlpha: 0 }); // Fade in the background image
+  gsap.set(menuItems, { opacity: 0, x: 50 }); // Slide in menu items with GPU optimization
 
   // Create the timeline, initially paused
   const tl = gsap.timeline({ paused: true, reversed: true });
 
-  tl
-    .to(modalMenu, {
-      x: 0,
-      opacity: 1,
-      display: "flex",
-      autoAlpha: 1,
-      duration: 0.5,
-      ease: "power2.inOut",
-    }, 0)
+  tl.to($modal, {
+    autoAlpha: 1, // Fade in the modal
+    display: "flex",
+    duration: 0.5,
+    ease: "power2.inOut",
+  }, 0)
     .to(bgImage, {
-      width: "60%",
+      autoAlpha: 1, // Fade in the background image
       duration: 0.4,
       ease: "power2.inOut",
     }, "<0.1")
-
-
     .fromTo(menuItems, {
-      opacity: 0,
-      x: 50,
+      opacity: 0, // Initial state for menu items
+      x: 50, // Slide in from the right
     }, {
-      opacity: 1,
-      x: 0,
+      opacity: 1, // Fade in menu items
+      x: 0, // Move to original position
       duration: 0.4,
       ease: "power2.out",
-      stagger: 0.1, 
+      stagger: 0.1, // Stagger for smoother appearance
     }, "-=0.1");
 
   return tl;
