fc4863d950f2c5de38f75de3d2dbb5c488fc28b6
sorozco <sergiorozco.ochoa@gmail.com>
2025-12-18 13:28:11 -0500

    Updating and protecting project

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from 'firebase/auth';
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCzOBiM8MYiBgb2yxk-AbxGn1OAAjm-5pI",
-  authDomain: "redil-digital.firebaseapp.com",
-  projectId: "redil-digital",
-  storageBucket: "redil-digital.firebasestorage.app",
-  messagingSenderId: "759627209374",
-  appId: "1:759627209374:web:a9d8ebe9f789992ce6f98f"
+  apiKey: import.meta.env.PRIVATE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.PRIVATE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.PRIVATE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.PRIVATE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.PRIVATE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.PRIVATE_FIREBASE_APP_ID
 };
 
 
