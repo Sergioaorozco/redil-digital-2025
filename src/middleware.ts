76342c3728de7c8ce6bc4dc6f8cf5cff4529cfa7
sorozco <sergiorozco.ochoa@gmail.com>
2026-01-05 18:02:56 -0500

    Creating get Initial helper and improving types

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,10 +5,17 @@ const isProtectedRoute = ['/miembros(.*)'];
 const loggedInRoutes = ['/ingreso'];
 
 export const onRequest = defineMiddleware(async ({url, locals, redirect}, next) => {
-  const isLoggedIn = !!firebaseApp.auth.currentUser;
+  const user = firebaseApp.auth.currentUser;
+  const isLoggedIn = !!user;
 
   locals.isLoggedIn = isLoggedIn;
   
+  locals.userInfo = isLoggedIn ? {
+    userName: user?.displayName,
+    userEmail: user?.email,
+    userPhoto: user.photoURL
+  }: null
+
   // Check if no logged user is trying to access a protected route
   if ( !isLoggedIn && isProtectedRoute.some(route => url.pathname.match(route))) {
     return redirect('/ingreso');
--- src/types.d.ts
+++ src/types.d.ts
@@ -1,7 +1,12 @@
-
+interface UserData {
+  userName?: string | null | undefined;
+  userEmail?: string | null | undefined;
+  userPhoto?: string | null | undefined;
+}
 
 declare namespace App {
-  interface Locals  {
+  interface Locals {
     isLoggedIn: boolean;
+    userInfo?: UserData | null;
   }
 }
