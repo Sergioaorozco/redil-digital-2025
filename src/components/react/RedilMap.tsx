54069212c0878eb0eb941bf9d268cc8f8fff3811
sorozco <sergiorozco.ochoa@gmail.com>
2026-01-12 10:10:40 -0500

    Improving map

--- src/components/react/RedilMap.tsx
+++ src/components/react/RedilMap.tsx
@@ -1,4 +1,6 @@
-import { Map, MapControls, MapMarker, MapPopup } from "@/components/ui/map";
+import { Map, MapControls, MapMarker, MapPopup, MarkerContent, MarkerTooltip } from "@/components/ui/map";
+import { MapPinIcon } from "lucide-react";
+import { useState } from "react";
 
 const locations = [
   {
@@ -13,10 +15,12 @@ const locations = [
     lng: -75.5965579075528,
     lat: 6.23967415351913
   },
-  { id: 3, name: "Super Mu La 33", lng: -75.59630186546296, lat: 6.239321588733043 },
+  { id: 3, name: "Super Mu La 33", lng: -75.5963384985285, lat: 6.239184559336569 },
 ];
 
 export default function RedilMap() {
+  const [popUp, setPopUp] = useState(false);
+
   return (
     <div className="h-full w-full rounded-lg overflow-hidden">
       <Map center={[-75.5961691028774, 6.240277472394894]} zoom={17}>
@@ -33,9 +37,23 @@ export default function RedilMap() {
             longitude={location.lng}
             latitude={location.lat}
           >
-            <MapPopup longitude={location.lng} latitude={location.lat}>
-              <p>{location.name}</p>
-            </MapPopup>
+            {location.name === 'Redil Laureles' ? (
+              <>
+                <MarkerContent>
+                  <MapPinIcon fill="#ff2424" className="text-red-800" />
+                </MarkerContent>
+                <MapPopup longitude={location.lng} latitude={location.lat}>
+                  <p>{location.name}</p>
+                </MapPopup>
+              </>
+            ) : (
+              <>
+                <MarkerContent>
+                  <div className="size-4 rounded-full bg-background border-2 border-white shadow-lg" />
+                </MarkerContent>
+                <MarkerTooltip>{location.name}</MarkerTooltip>
+              </>
+            )}
           </MapMarker>
         ))}
       </Map>
