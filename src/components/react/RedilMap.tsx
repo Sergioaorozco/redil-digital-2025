24cb6c917e38b13f88322de755f0462e898644e6
sorozco <sergiorozco.ochoa@gmail.com>
2026-01-11 20:25:11 -0500

    Correct map coordenates

--- src/components/react/RedilMap.tsx
+++ src/components/react/RedilMap.tsx
@@ -3,8 +3,14 @@ import { Map, MapControls } from "@/components/ui/map";
 export default function RedilMap() {
   return (
     <div className="h-[450px] w-full rounded-lg overflow-hidden">
-      <Map center={[-75.60223, 6.24296]} zoom={15}>
-        <MapControls />
+      <Map center={[6.240277472394894, -75.5961691028774]} zoom={17}>
+        <MapControls
+          position="bottom-right"
+          showZoom
+          showCompass
+          showLocate
+          showFullscreen
+        />
       </Map>
     </div>
   )
