0fdb94da30fa33c5cd837441beac02dc4e8e1f50
sorozco <sergiorozco.ochoa@gmail.com>
2026-01-11 20:33:07 -0500

    Improving coordenates

--- src/components/react/RedilMap.tsx
+++ src/components/react/RedilMap.tsx
@@ -4,22 +4,22 @@ const locations = [
   {
     id: 1,
     name: "Bomba los Almendros",
-    lng: 6.2387899130411215,
-    lat: -75.59638152299985
+    lng: -75.59638152299985,
+    lat: 6.2387899130411215
   },
   {
     id: 2,
     name: "Farmatodo",
-    lng: 6.23967415351913,
-    lat: -75.5965579075528
+    lng: -75.5965579075528,
+    lat: 6.23967415351913
   },
-  { id: 3, name: "Super Mu La 33", lng: 6.239321588733043, lat: -75.59630186546296 },
+  { id: 3, name: "Super Mu La 33", lng: -75.59630186546296, lat: 6.239321588733043 },
 ];
 
 export default function RedilMap() {
   return (
     <div className="h-[450px] w-full rounded-lg overflow-hidden">
-      <Map center={[6.240277472394894, -75.5961691028774]} zoom={17}>
+      <Map center={[-75.5961691028774, 6.240277472394894]} zoom={17}>
         <MapControls
           position="bottom-right"
           showZoom
