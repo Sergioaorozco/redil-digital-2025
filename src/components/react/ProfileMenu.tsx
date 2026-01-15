af98f27980afefced024c7151ec0780d93066e9c
sorozco <sergiorozco.ochoa@gmail.com>
2026-01-14 21:07:47 -0500

    Remove unused dependency

--- src/components/react/ProfileMenu.tsx
+++ src/components/react/ProfileMenu.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React, { useState, useRef, useEffect } from 'react';
+import { useState, useRef, useEffect } from 'react';
 import { actions } from "astro:actions";
 import { UserRound } from 'lucide-react';
 
