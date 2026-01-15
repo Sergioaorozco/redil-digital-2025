77399a8c604061f88f7b396667167d854fefaf12
sorozco <sergiorozco.ochoa@gmail.com>
2026-01-14 20:43:02 -0500

    Udpating texts within the accordeon

--- src/components/react/MinisteriosAccordion.tsx
+++ src/components/react/MinisteriosAccordion.tsx
@@ -41,12 +41,12 @@ type Ministerio = {
 const ministeriosData: Ministerio[] = [
   {
     title: "Bienvenida",
-    description: "Buscamos juntas la provisión de Cristo y un mayor entendimiento de la Verdad, y creemos que –como mujeres– podemos hacer la diferencia solo por Su gracia en Mí. Anhelamos con gratitud que este conocimiento nos lleve a reflejar la imagen de Cristo en nuestros diferentes entornos, en el amor, la compasión y la misericordia hacia otros.",
+    description: "Bienvenidos a la comunidad de la Iglesia Redil Laureles. Somos un grupo de personas que creemos en la importancia de la Iglesia y su papel en la sociedad. Somos un equipo de servidores que acompañamos a conocer y conectar con la Iglesia Redil Laureles.",
     images: getSortedImages(bienvenida)
   },
   {
     title: "Su Gracia en Mi",
-    description: "Buscamos juntas la provisión de Cristo y un mayor entendimiento de la Verdad, y creemos que –como mujeres– podemos hacer la diferencia solo por Su gracia en Mí. Anhelamos con gratitud que este conocimiento nos lleve a reflejar la imagen de Cristo en nuestros diferentes entornos, en el amor, la compasión y la misericordia hacia otros.",
+    description: "Buscamos juntas la provisión de Cristo y un mayor entendimiento de la Verdad, y creemos que como mujeres podemos hacer la diferencia solo por Su gracia en Mí. Anhelamos con gratitud que este conocimiento nos lleve a reflejar la imagen de Cristo en nuestros diferentes entornos, en el amor, la compasión y la misericordia hacia otros.",
     images: getSortedImages(mujeresImages)
   },
   {
@@ -71,7 +71,7 @@ const ministeriosData: Ministerio[] = [
   },
   {
     title: "Misiones",
-    description: "A través de la Misión Refugio en Granizal, cumplimos la misión de la iglesia en palabra y obra. Allí, compartimos el Evangelio y apoyamos el torneo femenino “Con el maltrato no trato”, un proyecto que protege a las niñas de riesgos sociales como el reclutamiento, la prostitución y las adicciones.",
+    description: "A través de la Misión Refugio en Granizal, cumplimos la misión de la iglesia en palabra y obra. Allí, compartimos el Evangelio y apoyamos el torneo de fútbol mixto “Para que tengas vida”, un proyecto que protege a los jóvenes de riesgos sociales como el reclutamiento, la prostitución y las adicciones.",
     images: getSortedImages(refugio)
   }
 ];
